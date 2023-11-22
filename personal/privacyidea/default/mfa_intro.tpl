gosa-project/gosa-plugins-privacyidea

commit 6f9f20162cc63a7b879f7f73e5f53431fe4e71c4

mfa_intro.tpl: Hide mfaRequired properties if user has no ACL permissions

personal/privacyidea/default/mfa_intro.tpl
@@ -44,6 +44,7 @@
     </p>
 </div>
 
+{if $mfaRequiredByRuleACL}
 {render acl=$mfaRequiredByRuleACL}
 <div class="row">
     <div class="col">
@@ -54,7 +55,9 @@
     </div>
 </div>
 {/render}
+{/if}
 {if ($parent != "roletabs")}
+{if $mfaRequiredByUserACL}
 {render acl=$mfaRequiredByUserACL}
 <div class="row">
     <div class="col">
@@ -67,6 +70,7 @@
     </div>
 </div>
 {/render}
+{/if}
 {/if}
 
 {if $allowedTokenTypesACL}

commit df3130895305d9e932cf36ccfbe870fdea3e6d06

mfa_intro.tpl: Fix up HTML
This fixes broken HTML introduced in 6f9f20162cc63a7b879f7f73e5f53431fe4e71c4.

personal/privacyidea/default/mfa_intro.tpl
@@ -42,8 +42,10 @@
         </b>.<br>
         {t}This happens due to organizational policy or user choice:{/t}
     </p>
+{/if}
 </div>
 
+{if !$mfaRequiredByRuleACL && !$mfaRequiredByUserACL && !$allowedTokenTypesACL && !$manageTokensACL}
 {if $mfaRequiredByRuleACL}
 {render acl=$mfaRequiredByRuleACL}
 <div class="row">